8817313c797997945c4f7c05a28788343d81b5e8
Abhinav <AbhinavSunil@hotmail.com>
2026-08-10 22:52:46 +0530

    Feat : Going to setup the DB Layer

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { 'simple-import-sort': simpleImportSort },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node }
+  },
+  pluginJs.configs.recommended
 ]);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { PORT } from './config/server.config.js';
 import { StatusCodes } from 'http-status-codes';
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'Hi Hello' });
 });
